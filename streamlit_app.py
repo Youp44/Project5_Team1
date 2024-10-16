0ef41c23dad5279ff4aafb68a4e79d4eaf49df46
Youp44 <527226@student.fontys.nl>
2024-10-16 15:22:45 +0200

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,25 @@ dataframe = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(dataframe.style.highlight_max(axis=0))
+# dit werkt dus voor correlatie
 
 
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
 
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
