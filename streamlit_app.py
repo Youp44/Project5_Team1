bb487863875d0b4895b6d20f50930af9098db10c
Youp44 <527226@student.fontys.nl>
2024-10-16 15:16:14 +0200

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,14 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 
-df = pd.DataFrame({
-    'first column': [1,2,3,4],
-    'second column': [10,20,30,40]
-})
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
-df
+
 st.title("Project 5 Omloopsplanning ")
 
-st.write("""
-         Ik baal van me leven
-    """
-)
 
-st.write("""
-         youpkakpoep
-    """
-)
+
+
