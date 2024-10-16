a0ec0ec24619d5aafb587f9fc18b13ffe20a52cb
Youp44 <527226@student.fontys.nl>
2024-10-16 15:17:27 +0200

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 
-st.write(pd.DataFrame({
+df = (pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-
-
+#of gewoon met normale df 
+st.table(df)
 st.title("Project 5 Omloopsplanning ")
 
 
